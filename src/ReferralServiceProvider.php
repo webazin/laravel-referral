bdf30b64bee398287f4c7c1933f635590d866887
webazin net <farhadpour4187@gmail.com>
2024-01-03 12:45:21 +0330

    change directories path

--- src/ReferralServiceProvider.php
+++ src/ReferralServiceProvider.php
@@ -8,7 +8,7 @@ class ReferralServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->loadRoutesFrom(__DIR__ . 'routes/referral.php');
+        $this->loadMigrationsFrom(__DIR__ . '../database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '../routes/referral.php');
     }
 }
