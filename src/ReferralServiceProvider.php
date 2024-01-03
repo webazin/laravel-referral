a99b6fd83c59ae1c8d3e5390bb9e1bc3170950be
webazin net <farhadpour4187@gmail.com>
2024-01-03 13:05:15 +0330

    update path

--- src/ReferralServiceProvider.php
+++ src/ReferralServiceProvider.php
@@ -8,7 +8,7 @@ class ReferralServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '../database/migrations');
-        $this->loadRoutesFrom(__DIR__ . '../routes/referral.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/referral.php');
     }
 }
